3cc7280c4fd931498a2f731de8fef2196b3b2a64
Haruwa <jaimemeireles@gmail.com>
2020-10-08 22:55:09 -0300

    Adicionando MotoView.java

---
+++ MotoView.java
@@ -0,0 +1,7 @@
+public class MotoView {
+    public void printMotorcycle(String mPlate, String mModel, String mColor){
+        System.out.println("Placa da moto: " + mPlate);
+        System.out.println("Modelo da moto: " + mModel);
+        System.out.println("Cor da moto: " + mColor);
+    }
+}
